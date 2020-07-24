4947b9ba50eca974d3f8236ca83c52639d646e7e
Embry <jdanembry@gmail.com>
2020-07-24 16:29:39 -0600

    First commit

---
+++ Repository/OwnerRepository.cs
@@ -0,0 +1,56 @@
+﻿using Contracts;
+using Entities;
+using Entities.ExtendedModels;
+using Entities.Extensions;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class OwnerRepository : RepositoryBase<Owner>, IOwnerRepository
+    {
+        public OwnerRepository(RepositoryContext repositoryContext)
+            :base(repositoryContext)
+        {
+
+        }
+
+        public void CreateOwner(Owner owner)
+        {
+            owner.Id = Guid.NewGuid();
+            Create(owner);
+            Save();
+        }
+
+        public IEnumerable<Owner> GetAllOwners()
+        {
+            return FindAll().OrderBy(o => o.Name);
+        }
+
+        public Owner GetOwnerById(Guid ownerId)
+        {
+            return FindByCondition(o => o.Id.Equals(ownerId))
+                .DefaultIfEmpty(new Owner())
+                .FirstOrDefault();
+        }
+
+        public OwnerExtended GetOwnerWithDetails(Guid ownerId)
+        {
+            return new OwnerExtended(GetOwnerById(ownerId))
+            {
+                Accounts = RepositoryContext.Accounts.Where(a => a.OwnerId == ownerId)
+            };
+        }
+
+        public void UpdateOwner(Owner dbOwner, Owner owner)
+            {
+                dbOwner.Map(owner);
+                Update(dbOwner);
+                Save();
+            }
+        }
+    }
+
